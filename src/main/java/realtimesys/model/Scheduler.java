9e0714eef364f43324a8c923b6cb98113b738738
Lan Tran <ltran@amperecomputing.com>
2019-12-28 08:47:26 +0700

    Added basic scheduler for LST

--- src/main/java/realtimesys/model/Scheduler.java
+++ src/main/java/realtimesys/model/Scheduler.java
@@ -20,3 +20,62 @@ public interface Scheduler {
     void startSchedule();
     Schedule getSchedule();
 }
+class LSTScheduling implements Scheduler
+{
+    Schedule LSTschedule = new Schedule();
+    public void setTasks(List<Task >tasks)
+    {
+        
+    }
+    public Schedule getSchedule()
+    {
+        return LSTschedule; 
+    }
+    public void startSchedule(final List<PeriodicTask> taskList)
+    {
+        double lcm = calcLCM(taskList);
+        
+        for(int timeUnit = 0; timeUnit < lcm; timeUnit++) 
+        {
+            double minSlacktime = lcm;
+            PeriodicTask highestPriTask = new PeriodicTask();
+            for(PeriodicTask t : taskList) 
+            {
+                if ((timeUnit == t.rDeadline) && (t.e != 0))
+                {
+                    
+                }
+                else if ((timeUnit == t.rDeadline) && (t.e == 0))
+                {
+                    t.phase = timeUnit;
+                }
+                double slacktime = t.phase + t.getRDeadline() - (timeUnit + t.e);
+                if (minSlacktime > slacktime)
+                {
+                    minSlacktime = slacktime;
+                    highestPriTask = t;
+                }
+            }
+            // Decrease the execution time of the highest pri task.
+            highestPriTask.e = highestPriTask.e - 1;
+            // Add current task into the task list.
+            LSTschedule.getJobAssignments().add(l);
+        }
+    }
+    static double calcLCM(List<PeriodicTask> taskList) {
+
+        double lcm = taskList.get(0).getPeriod();
+        for(boolean flag = true; flag; ) {
+            for(PeriodicTask x : taskList) {
+                if(lcm % x.getPeriod() != 0) {
+                    flag = true;
+                    break;
+                }
+                flag = false;
+            }
+            lcm = flag? (lcm + 1) : lcm;
+        }
+        return lcm;
+
+    }
+}
